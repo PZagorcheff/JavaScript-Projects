644b402db6f4a7a346e9f8fdce33c0aba37392e5
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-05-04 12:21:24 +0100

    Project2_functions added
    Two new exercises are added to Project2_functions:
    - v.1 - Date but in separated files
    - v.2 - Concatenation using variable and string

---
+++ Basic JavaScript Projects/Project2_functions/v.1 - Date separated files/JS/main.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+    var x = document.getElementById('demo').innerHTML=Date();
+  }
+
+window.alert("If you want to see your current date & time click the button \"Time\" below");
---
+++ Basic JavaScript Projects/Project2_functions/v.2 - Concatenation/JS/main.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+    var example = "Hello!"; //Variable declaration
+    example += " I'm concatenated text."; //Variable conctenated with string
+    document.getElementById("Concatenate").innerHTML = example; //Change of text
+  }
