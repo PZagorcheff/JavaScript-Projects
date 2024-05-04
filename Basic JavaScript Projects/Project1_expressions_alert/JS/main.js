9bc6a2d38c7b13ed89e98fa81b40cc6fe32df02f
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-05-04 10:12:30 +0100

    Basic JS exercise
    - Concatenation
    - fontcolor()

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,16 @@
-document.write ("Hello Mister P222");
+var city_1 = "London", city_2 = "Sofia", city_3 = "Birmingham";
+var city_1 = city_1.fontcolor("blue"); //blue color
+var city_2 = city_2.fontcolor("red"); // red color
+var city_3 = city_3.fontcolor("green"); // green color
+
+//Text starts
+var color_1 = " is in blue, ";
+var color_2 = " is in red, ";
+var color_3 = " is in green!";
+
+// Concatinations start
+document.write(city_1 + color_1);
+document.write(city_2 + color_2);
+document.write(city_3 + color_3);
+
+window.alert("Hello! Are you ready to see cities written in different font colors?");
