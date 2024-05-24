3de2802afa844cd80faacfb76c6f8fd6776af8a1
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-05-24 18:18:29 +0100

    JS Logical operators
    JS Logical operators added

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,35 @@
+function js_Function_1() {
+  document.write(typeof 5);
+}
+
+function js_Function_2() {
+  document.getElementById("JS_F").innerHTML = "one"+2+"three"+4;
+}
+
+function js_Function_3_comparison() {
+  document.write(10 == 20);
+}
+
+function js_Function_4_three_comp() {
+  document.write(10 === 10);
+}
+
+function js_Function_5_greater() {
+  document.write(10 > 10);
+}
+
+function js_Function_6_lesser() {
+  document.write(10 < 8);
+}
+
+function js_Function_7_AND() {
+  document.write(7 < 8 && 4<8);
+}
+
+function js_Function_8_OR() {
+  document.write(7 < 8 || 4<8);
+}
+
+function js_Function_9_NOT() {
+  document.getElementById("JS_F").innerHTML = !(7 > 8 || 4>8);
+}
