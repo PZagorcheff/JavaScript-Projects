be5dbe86196d0ef4f339802f25fb54d54a9fd06d
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-05-29 16:02:26 +0100

    String methods added
    - concat()
    - slice(),
    - toString(),
    - toPrecision()

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,55 @@
+function full_sentence() {
+  var p1 = 12938.3012987376112;
+  
+  document.getElementById("ex_string_methods").innerHTML = p1.toFixed(2);
+}
+
+/* Assignment 44
+function full_sentence() {
+  var p1 = "I have ";
+  var p2 = "made this ";
+  var p3 = "into a complete ";
+  var p4 = "sentence.";
+  
+  var whole = p1.concat(p2, p3, p4);
+  document.getElementById("concat_test").innerHTML = whole;
+ End of Assignment 44
+ */
+
+ /* Assignment 45
+function full_sentence() {
+  var p1 = "I have made this!";
+  var p2 = p1.slice(2,7);
+  
+  document.getElementById("ex_string_methods").innerHTML = p2;
+}
+ End of Assignment 45
+ */
+
+  /* Challenge # 10
+function full_sentence() {
+  var p1 = "I have made this";
+  var p2 = p1.toUpperCase();
+  
+  document.getElementById("ex_string_methods").innerHTML = p2;
+}
+ End of Challenge # 10
+ */
+
+ /* Assignment 46
+function full_sentence() {
+  var p1 = 182;
+  
+  document.getElementById("ex_string_methods").innerHTML = p1.toString();
+}
+ End of Assignment 46
+ */
+
+  /* Assignment 47
+function full_sentence() {
+  var p1 = 12938.3012987376112;
+  
+  document.getElementById("ex_string_methods").innerHTML = p1.toPrecision(10);
+}
+ End of Assignment 47
+ */
