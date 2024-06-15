824037caa66ec815e5d1a0e993d44ca9a3e10235
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-06-15 11:24:38 +0100

    JS Slideshow Working version
    - New JS slideshow working version added
    - Removed unused files

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,64 @@
+
+/*
+//Assignmrnt #55 - Objects
+let car = {
+  make: "Dodge ",
+  model: "Viper ",
+  year: "2021",
+  color: "red",
+  description: function() {
+    return "The car is a " + this.year + this.color + this.make + this.model;
+  }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+====================================================================================================
+//Challenge #14 - Return
+document.getElementById("demo").innerHTML = myFuction("John");
+
+function myFuction(name) {
+return "Hello " + name;
+}
+====================================================================================================
+Assignment #54 - Let - streigth in the HTML + <script> tags
+var X = 82;
+document.write(X);
+{
+    var X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+====================================================================================================
+Assignment #53 - Constant
+function constant_function() {
+  const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+  Musical_Instrument.color = "blue";
+  Musical_Instrument.price = "£700";
+  Musical_Instrument.newprice = "£1500"
+
+  document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type +
+  " was " + Musical_Instrument.price + ". But the new price is " + Musical_Instrument.newprice;
+}
+====================================================================================================
+Assignment #52 - Array
+  function array_Function() {
+    var Car = [];
+    Car[0] = "Ford";
+    Car[1] = "Mustang";
+    Car[2] = 1967;
+    Car[3] = "Convirtable";
+
+    document.getElementById("Array").innerHTML = "This " + Car[1] +
+    " is perfect example of " + Car[3] + " classic muscle car.";
+  }
+====================================================================================================
+ Assignment #51 - For Loops
+  var Instruments = ["Guitar", "Drums", "Piano", "Bass"];
+  var Content="";
+  var Y;
+
+  function for_Loop() {
+    for (Y=0; Y<Instruments.length; Y++) {
+      Content += Instruments[Y] + "</br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML=Content;
+  } */
